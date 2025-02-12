80e1a190d9c10bb9a0048bf5d1fd841fa7200b95
Anthony Te <112227149+AnthonyJTe@users.noreply.github.com>
2025-02-12 21:05:34 +0000

    added sorting.c

---
+++ sorting.c
@@ -0,0 +1,14 @@
+// Names: Navid Nikoo, Kasim Rashid, and Anthony Te
+// This program will take a list of intergers and divide them into two(almost) equally sized list
+// and two worker threads which will be sorting the list then merged with a merging thread
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <pthread.h>
+
+int main() {
+    // the list that will be sorted
+    int list[11] = {7, 12, 19, 3, 18, 4, 2, -5, 6, 15, 8};
+}
